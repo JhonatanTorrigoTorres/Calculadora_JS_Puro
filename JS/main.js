831e6eca85461bd21208f65a6a2c110d5b5fffbf
Jhonatan Torrigo Torres <jhonatanttorres@gmail.com>
2021-11-10 13:53:19 -0300

    Organização
    Padronizando a estrutura

--- JS/main.js
+++ JS/main.js
@@ -1,28 +1,17 @@
-//Função dos botões números//
-
 function insert(num)
 {
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
 }
-
-//Função do Botão de limpeza//
-
 function clean()
 {
     document.getElementById('resultado').innerHTML = "";
 }
-
-//Função do Botão de apagar//
-
 function back()
 {
     var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0,resultado.length -1)
 }
-
-//Função do Botão de calcular//
-
 function calcular()
 {
     var resultado = document.getElementById('resultado').innerHTML;
